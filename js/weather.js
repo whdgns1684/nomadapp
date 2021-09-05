90558cac6014922a0c902479ce9ff86869f70f65
whdgns1684 <89574543+whdgns1684@users.noreply.github.com>
2021-09-05 17:02:53 +0900

    Add files via upload

---
+++ js/weather.js
@@ -0,0 +1,19 @@
+const API_KEY = "8b0c0adedf66f705d3038e86b1712086";
+
+function onGeoOk(position){
+    const lat = position.coords.latitude;
+    const lng = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`;
+    fetch(url).then(response => response.json()).then(data => {
+        const weather = document.querySelector("#weather span:first-child");
+        const city = document.querySelector("#weather span:last-child");
+        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+        city.innerText = data.name;
+    });
+}
+
+function onGeoError() {
+    alert("Can't find you. No weather for you")
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
